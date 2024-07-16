d14167af925e1bc088c4be16b243df330591c73b
TIWIMIN <tiwimin@gmail.com>
2024-07-15 17:47:54 -0700

    Arrange elements in 4x4 grid

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const squares = []; 
+const row = [];
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i ++) {
-    squares[i] = document.createElement("div");
-    squares[i].textContent = "hi";
-    container.appendChild(squares[i]); 
-    squares[i].classList.add("square");
+// Need divs for each row to flex horizontally
+for (let i = 0; i < 4; i++) {
+    row[i] = document.createElement("div");
+    row[i].classList.add("row");
+    container.appendChild(row[i]);
 }
+
+for (let i = 0; i < 16; i++) {
+    squares[i] = document.createElement("div");
+    squares[i].textContent = "hi";; 
+    squares[i].classList.add("square");
+
+    if (i < 4) {
+        row[1].appendChild(squares[i]);
+    }
+    else if (i < 8) {
+        row[2].appendChild(squares[i]);
+    }
+    else if (i < 12) {
+        row[3].appendChild(squares[i]);
+    }
+    else {
+        row[4].appendChild(squares[i]);
+    }
+}
+
