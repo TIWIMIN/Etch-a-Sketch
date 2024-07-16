725867df4c45a59cc1dc421b2e1f488abab9e78f
TIWIMIN <tiwimin@gmail.com>
2024-07-15 18:28:45 -0700

    Create dynamic 16 x 16 grid

--- script.js
+++ script.js
@@ -3,28 +3,20 @@ const row = [];
 const container = document.querySelector("#container");
 
 // Need divs for each row to flex horizontally
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     row[i] = document.createElement("div");
     row[i].classList.add("row");
     container.appendChild(row[i]);
 }
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     squares[i] = document.createElement("div");
-    squares[i].textContent = "hi";; 
+    squares[i].textContent = i; 
     squares[i].classList.add("square");
 
-    if (i < 4) {
-        row[1].appendChild(squares[i]);
-    }
-    else if (i < 8) {
-        row[2].appendChild(squares[i]);
-    }
-    else if (i < 12) {
-        row[3].appendChild(squares[i]);
-    }
-    else {
-        row[4].appendChild(squares[i]);
-    }
+    let index = Math.floor(i / 16); 
+
+    console.log(index);
+    row[index].appendChild(squares[i]);
 }
 
