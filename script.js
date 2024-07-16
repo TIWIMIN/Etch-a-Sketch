cb23fbbcc66af57ff814d2a820d24394eb851cca
TIWIMIN <tiwimin@gmail.com>
2024-07-15 17:34:44 -0700

    Create square elements

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const squares = [];
 const container = document.querySelector("#container");
 
 for (let i = 0; i < 16; i ++) {
-    squares[i] = document.createElement();
-    container.appendChild(sqaures[i]); 
+    squares[i] = document.createElement("div");
+    squares[i].textContent = "hi";
+    container.appendChild(squares[i]); 
+    squares[i].classList.add("square");
 }
