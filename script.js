03f757051386a76d4d42c91ac82552c9516c4ece
TIWIMIN <tiwimin@gmail.com>
2024-07-15 17:15:38 -0700

    Create empty files

---
+++ script.js
@@ -0,0 +1,7 @@
+const squares = []; 
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i ++) {
+    squares[i] = document.createElement();
+    container.appendChild(sqaures[i]); 
+}
