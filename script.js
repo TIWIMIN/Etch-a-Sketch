f7f69ef1643ca289cc2d70e8183795dc2d79057c
TIWIMIN <tiwimin@gmail.com>
2024-07-16 18:23:04 -0700

    Add reset button functionality and dynamic sizing

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const squares = []; 
-const row = [];
+let squares = []; 
+let row = [];
 const container = document.querySelector("#container");
 const reset = document.querySelector("button");
 
 function createBoard(dim) {
+    if (squares.length > 0 && row.length > 0) {
+        squares.forEach((e) => e.remove());
+        row.forEach((e) => e.remove());
+        squares = [];
+        row = [];
+    }
+
     // Need divs for each row to flex horizontally
     for (let i = 0; i < dim; i++) {
         row[i] = document.createElement("div");
@@ -30,4 +37,17 @@ function createBoard(dim) {
 
 createBoard(16);
 
-//reset.addEventListener("click", () => resetboad)
+reset.addEventListener("click", () => {
+    let dim = prompt("Please choose the dimension of the new grid");
+    let validDim = false;
+    while (!validDim) {
+        if (dim > 100 || dim < 1) {
+            dim = prompt("Please enter a dimension less than 100 and greater than 1");
+        }
+        else {
+            validDim = true; 
+        }
+    }
+    createBoard(dim);
+})
+
